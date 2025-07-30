a19e1b7dcfd251d387003e2163ca3e13b2f4289b
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-30 13:01:35 -0300

    Feat: Create Delete Users

--- src/modules/users/user.controllers.ts
+++ src/modules/users/user.controllers.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 
 @Controller('users')
@@ -6,22 +6,27 @@ export class UserController {
     constructor(private userService: UserService) {}
 
     @Get()
-    async list() {
-        return await this.userService.list();
+    list() {
+        return this.userService.list();
     }
 
     @Get(':id')
-    async show(@Param('id') id: string) {
-        return await this.userService.show(id);
+    show(@Param('id') id: string) {
+        return this.userService.show(id);
     }
 
     @Post()
-    async createUser(@Body() body: any) {
-        return await this.userService.createUser(body);
+    createUser(@Body() body: any) {
+        return this.userService.createUser(body);
     }
 
     @Patch(':id')
-    async updateUser(@Param('id') id: string, @Body() body: any) {
-        return await this.userService.updateUser(id, body);
+    updateUser(@Param('id') id: string, @Body() body: any) {
+        return this.userService.updateUser(id, body);
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: string) {
+        return this.userService.deleteUser(id);
     }
 }
--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,24 +1,46 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { User } from "@prisma/client";
+
 
 @Injectable()
 export class UserService {
     constructor(private readonly prisma: PrismaService) {}
 
-    createUser(body: any): Promise<User> {
-        return this.prisma.user.create({ data: body });
-    }
     list() {
         return this.prisma.user.findMany();
     }
-    show(id: string) {
-        return this.prisma.user.findUnique({ where: { id: Number(id) } });
+    createUser(body: any): Promise<User> {
+        return this.prisma.user.create({ data: body });
     }
-    updateUser(id: string, body: any) {
+    async show(id: string) {
+        const user = await this.prisma.user.findUnique({ where: { id: Number(id) } });
+
+        if (!user) {
+            throw new NotFoundException(`User not found`);
+        }
+
+        return user;
+    }
+    async updateUser(id: string, body: any) {
+        const user = await this.prisma.user.findUnique({ where: { id: Number(id) } });
+
+        if (!user) {
+            throw new NotFoundException(`User not found`);
+        }
+
         return this.prisma.user.update({
             where: { id: Number(id) },
             data: body
         });
+    }
+    async deleteUser(id: string) {
+        const user = await this.prisma.user.findUnique({ where: { id: Number(id) } });
+
+        if (!user) {
+            throw new NotFoundException(`User not found`);
+        }
+
+        return this.prisma.user.delete({ where: { id: Number(id) } });
     }
 }
