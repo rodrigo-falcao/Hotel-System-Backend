c319086259afc880d1fdbf8c6314879ac983d477
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-07-31 15:33:16 -0300

    Feat:Create Login with Jwt auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,15 @@
 import { AuthLoginDTO } from "./domain/dto/authLogin.dto";
 import { AuthService } from "./auth.service";
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
-
+    
     @Post('login')
+    @HttpCode(200)
     login(@Body() body: AuthLoginDTO) {
         return this.authService.login(body);
     }
+    
 }
